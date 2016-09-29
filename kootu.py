be307d37cb51294ba95c455403633764fb47f3e8
sathiya <sathiyawiz@gmail.com>
2016-09-28 23:39:15 -0500

    First coded recipe

--- kootu.py
+++ kootu.py
@@ -1,7 +1,6 @@
 from utils.pantry import Ingredients
 from utils.cookingMethods import *
 
-
 oil = Ingredients.add("Vegetable oil", "2", "tablespoon")
 dhal = Ingredients.add("Toor dhal", "1.5", "cup")
 salt = Ingredients.add("Salt", "to taste", "")
@@ -11,7 +10,7 @@ chillies = Ingredients.add("Green chilli peppers", "8")
 tomatoes = Ingredients.add("Ripe tomatoes", "3")
 turmeric = Ingredients.add("Turmeric powder", "0.25", "tespoon")
 asafoetida = Ingredients.add("Asafoetida or heng powder", "0.25", "teaspoon")
-#Cut the vegetable in small cubes
+#Cut the vegetable in small cubes, can use cabbage instead
 vegetable = Ingredients.add("Squash", "1")
 #For final tempering
 channaDhal = Ingredients.add("Channa dhal", "2", "teaspoon")
@@ -22,13 +21,12 @@ Ingredients.acquireAll()
 
 #Use water to dhal ratio of 1:2
 cookedMixture = cook(dhal,tomatoes,turmeric,oil)
-
 simmeringMixture = simmer(cookedMixture)
+
 simmeringMixture = add(simmeringMixture, vegetable, salt)
 
 cumin = fry(cumin)
 blendedMixture = blend(cumin, chillies, coconut)
-
 simmeringMixture = add(simmeringMixture, blendedMixture)
 
 temperMix = temper(mustard, channaDhal, curryLeaves)
